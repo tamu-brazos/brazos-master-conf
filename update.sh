d3303fab2b623f01d74c715af456c93aaa06027e
Trey Dockendorf <treydock@gmail.com>
2014-05-20 13:12:04 -0500

    * Add update.sh to help automate the updating of librarian-puppet in production locations * Update lmod module

---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+exit_if_failed() {
+  retval=$?
+
+  if [ $retval -ne 0 ]; then
+    echo "Exit code: ${retval}"
+    exit $retval
+  fi
+}
+
+echo "Executing: git remote update"
+git remote update
+exit_if_failed $?
+
+echo "Executing: git pull origin master"
+git pull origin master
+exit_if_failed $?
+
+echo "Executing: bundle install"
+bundle install
+exit_if_failed $?
+
+echo "Executing: bundle exec librarian-puppet install"
+bundle exec librarian-puppet install
+
+echo "Fixing files lacking user write bit"
+find modules -type f ! -perm -u+w -exec chmod u+w {} \;
+
+echo "Fixing files lacking group read bit"
+find modules -type f ! -perm -g+r -exec chmod g+r {} \;
+
+echo "Fixing files lacking other read bit"
+find modules -type f ! -perm -o+r -exec chmod o+r {} \;
+
+exit 0
